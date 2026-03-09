80a93ea262a63b78c2cd87a2feec9602d161d377
tomas96ech <techeverri96@gmail.com>
2026-03-08 20:29:37 -0500

    initial commit

---
+++ src/main/java/com/school/roller_speed/RollerSpeedApplication.java
@@ -0,0 +1,13 @@
+package com.school.roller_speed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollerSpeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollerSpeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/school/roller_speed/RollerSpeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.roller_speed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RollerSpeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
